03813f293193759cdd2f6ec92ef5b361e4fbeff8
Irakli Gozalishvili <rfobic@gmail.com>
2018-03-02 23:52:07 -0800

    Implement Heading node view.

---
+++ src/UI/Allusion/NodeView/Heading.js
@@ -0,0 +1,36 @@
+// @flow strict
+
+import type { Mark, Node, Schema } from "prosemirror-model"
+import { Decoration, DecorationSet, EditorView } from "prosemirror-view"
+
+export default class HeadingView {
+  dom: Element
+  contentDOM: Element
+  mark: HTMLElement
+  editor: EditorView
+  static new(node: Node, editor: EditorView) {
+    return new HeadingView(node, editor)
+  }
+  constructor(node: Node, editor: EditorView) {
+    const level = node.attrs.level || 1
+    const tagName = `h${level}`
+    const dom = editor.root.createElement(tagName)
+    const mark = editor.root.createElement("mark")
+    const content = editor.root.createElement("span")
+    mark.textContent = `${"#".repeat(level)} `
+    dom.appendChild(mark)
+    dom.appendChild(content)
+    this.dom = dom
+    this.contentDOM = content
+    this.mark = mark
+    this.update(node)
+  }
+  update(node: Node) {
+    if (node.attrs.expand) {
+      this.dom.classList.add("expand")
+    } else {
+      this.dom.classList.remove("expand")
+    }
+    return true
+  }
+}
