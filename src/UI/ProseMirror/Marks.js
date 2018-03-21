3f31ff9671b0320a372dabb3d83374f8ca25b7b2
Irakli Gozalishvili <rfobic@gmail.com>
2018-03-21 15:22:39 -0700

    Add functions for markup range discovery.

--- src/UI/ProseMirror/Marks.js
+++ src/UI/ProseMirror/Marks.js
@@ -45,6 +45,79 @@ export const getMarkersAt = (at: ResolvedPos): Marker[] => {
   return markers
 }
 
+export const getMarkupMarksAround = (at: ResolvedPos): Mark[] => {
+  const markers = []
+  const { parent, pos, nodeBefore, nodeAfter } = at
+  const index = at.index()
+  const marksAt = at.marks()
+  const marksBefore = nodeBefore ? nodeBefore.marks : empty
+  const marksAfter = nodeAfter ? nodeAfter.marks : empty
+  const marks = new Set()
+  for (const mark of [...marksAt, ...marksBefore, ...marksAfter]) {
+    if (mark.attrs.markup != null) {
+      marks.add(mark)
+    }
+  }
+  return [...marks]
+}
+
+export const findMarkupRange = (position: ResolvedPos): [number, number] => {
+  const marks = getMarkupMarksAround(position)
+  return [
+    findMarkupRangeStart(position, marks),
+    findMarkupRangeEnd(position, marks)
+  ]
+}
+
+export const findMarkupRangeStart = (
+  position: ResolvedPos,
+  marks: Mark[]
+): number => {
+  const { parent, pos, textOffset } = position
+  let offset = pos - textOffset
+  let childIndex = position.index() - 1
+  let markIndex = marks.length
+
+  let child = childIndex > 0 ? parent.child(childIndex) : null
+  let mark = marks[--markIndex]
+
+  while (child && mark) {
+    if (child.marks.includes(mark)) {
+      offset = offset - child.nodeSize
+      child = childIndex > 0 ? parent.child(--childIndex) : null
+    } else {
+      mark = markIndex > 0 ? marks[--markIndex] : null
+    }
+  }
+
+  return offset
+}
+
+export const findMarkupRangeEnd = (
+  position: ResolvedPos,
+  marks: Mark[]
+): number => {
+  const { parent, pos, textOffset } = position
+  let offset = pos - textOffset
+  let childIndex = position.index()
+  const { childCount } = parent
+  let markIndex = marks.length
+
+  let child = childIndex < childCount ? parent.child(childIndex) : null
+  let mark = marks[--markIndex]
+
+  while (child && mark) {
+    if (child.marks.includes(mark)) {
+      offset = offset + child.nodeSize
+      child = ++childIndex < childCount ? parent.child(childIndex) : null
+    } else {
+      mark = markIndex > 0 ? marks[--markIndex] : null
+    }
+  }
+
+  return offset
+}
+
 export const findMarkStart = (
   node: Node,
   pos: number,
