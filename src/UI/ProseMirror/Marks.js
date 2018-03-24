20698ee46b84669ebc966d3f1579a1dd7ae8b23f
Irakli Gozalishvili <rfobic@gmail.com>
2018-03-23 23:39:36 -0700

    Detect markup ranges by markup rather than marks.
    It does not make sense to apply same markup over the whole range instead same marks could be used.

--- src/UI/ProseMirror/Marks.js
+++ src/UI/ProseMirror/Marks.js
@@ -1,8 +1,9 @@
 // @flow strict
 
 import type { Transaction, Selection } from "prosemirror-state"
-import type { MarkType, ResolvedPos, Mark, Node } from "prosemirror-model"
+import type { MarkType, ResolvedPos, Node } from "prosemirror-model"
 import { Decoration, DecorationSet } from "prosemirror-view"
+import { Mark } from "prosemirror-model"
 
 export type Marker = {
   mark: Mark,
@@ -10,52 +11,18 @@ export type Marker = {
   end: number
 }
 
-export const getSelectionMarkers = (selection: Selection): Marker[] => {
-  const { $cursor, $from, $to } = selection
-  if ($cursor) {
-    return getMarkersAt($cursor)
-  } else {
-    return getMarkersInRange($from, $to)
-  }
-}
-
-export const getMarkersInRange = (
-  from: ResolvedPos,
-  to: ResolvedPos
-): Marker[] => {
-  const markers = []
-  return markers
-}
-
-const empty = []
-export const getMarkersAt = (at: ResolvedPos): Marker[] => {
-  const markers = []
-  const { parent, pos, nodeBefore, nodeAfter } = at
-  const index = at.index()
-  const marksUnder = at.marks()
-  const marksBefore = nodeBefore ? nodeBefore.marks : empty
-  const marksAfter = nodeAfter ? nodeAfter.marks : empty
-  const marks = new Set([...marksUnder, ...marksBefore, ...marksAfter])
-  for (const mark of marks) {
-    const start = findMarkStart(parent, pos - at.textOffset, index, mark)
-    const end = findMarkEnd(parent, pos - at.textOffset, index, mark)
-    markers.push({ start, end, mark })
-  }
-
-  return markers
-}
-
-export const getMarkupMarksAround = (at: ResolvedPos): Mark[] => {
+export const getMarkupMarksAround = (at: ResolvedPos): string[] => {
   const markers = []
   const { parent, pos, nodeBefore, nodeAfter } = at
   const index = at.index()
   const marksAt = at.marks()
-  const marksBefore = nodeBefore ? nodeBefore.marks : empty
-  const marksAfter = nodeAfter ? nodeAfter.marks : empty
+  const marksBefore = nodeBefore ? nodeBefore.marks : Mark.none
+  const marksAfter = nodeAfter ? nodeAfter.marks : Mark.none
   const marks = new Set()
   for (const mark of [...marksAt, ...marksBefore, ...marksAfter]) {
-    if (mark.attrs.markup != null) {
-      marks.add(mark)
+    const markup = mark.attrs.markup || ""
+    if (markup != "") {
+      marks.add(markup)
     }
   }
   return [...marks]
@@ -71,18 +38,18 @@ export const findMarkupRange = (position: ResolvedPos): [number, number] => {
 
 export const findMarkupRangeStart = (
   position: ResolvedPos,
-  marks: Mark[]
+  marks: string[]
 ): number => {
   const { parent, pos, textOffset } = position
   let offset = pos - textOffset
   let childIndex = position.index() - 1
   let markIndex = marks.length
 
-  let child = childIndex > 0 ? parent.child(childIndex) : null
+  let child = childIndex >= 0 ? parent.child(childIndex) : null
   let mark = marks[--markIndex]
 
   while (child && mark) {
-    if (child.marks.includes(mark)) {
+    if (child.marks.some($ => $.attrs.markup === mark)) {
       offset = offset - child.nodeSize
       child = childIndex > 0 ? parent.child(--childIndex) : null
     } else {
@@ -95,7 +62,7 @@ export const findMarkupRangeStart = (
 
 export const findMarkupRangeEnd = (
   position: ResolvedPos,
-  marks: Mark[]
+  marks: string[]
 ): number => {
   const { parent, pos, textOffset } = position
   let offset = pos - textOffset
@@ -107,7 +74,7 @@ export const findMarkupRangeEnd = (
   let mark = marks[--markIndex]
 
   while (child && mark) {
-    if (child.marks.includes(mark)) {
+    if (child.marks.some($ => $.attrs.markup === mark)) {
       offset = offset + child.nodeSize
       child = ++childIndex < childCount ? parent.child(childIndex) : null
     } else {
@@ -115,44 +82,5 @@ export const findMarkupRangeEnd = (
     }
   }
 
-  return offset
-}
-
-export const findMarkStart = (
-  node: Node,
-  pos: number,
-  index: number,
-  mark: Mark
-): number => {
-  let n = index - 1
-  let offset = pos
-  while (n >= 0) {
-    let target = node.child(n--)
-    if (target && target.marks.includes(mark)) {
-      offset = offset - target.nodeSize
-    } else {
-      break
-    }
-  }
-  return offset
-}
-
-export const findMarkEnd = (
-  node: Node,
-  pos: number,
-  index: number,
-  mark: Mark
-): number => {
-  const count = node.childCount
-  let n = index
-  let offset = pos
-  while (n < count) {
-    let target = node.child(n++)
-    if (target && target.marks.includes(mark)) {
-      offset = offset + target.nodeSize
-    } else {
-      break
-    }
-  }
   return offset
 }
